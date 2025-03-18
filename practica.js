042178aa0626fdc5355607909860387901dd2bc7
alumno <alumno@Instituto.local>
2025-03-18 09:51:41 -0300

    condicional clase 18/3

--- practica.js
+++ practica.js
@@ -5,3 +5,10 @@ let añoNacimiento = 2009
 let añoActual = 2025
 
 alert('Mi edad es' + añoActual - añoNacimiento)
+
+let edad 
+if (edad >= 17){
+    alert("Podes ingresar al boliche")
+}else{
+    alert("no podes ingresar, lo siento")
+}
