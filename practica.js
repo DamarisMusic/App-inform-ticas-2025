3bea109245b8d588b026c92da415fae22386eda3
alumno <alumno@Instituto.local>
2025-07-30 08:18:46 -0300

    clase 30

--- practica.js
+++ practica.js
@@ -1,5 +1,5 @@
 
-
+/*
 let añoNacimiento = 2009
 
 let añoActual = 2025
@@ -21,3 +21,39 @@ if((lluvia == 'si')&&(nublado == 'si')){
 }else{
     alert('¿Tomamos unos mates?')
 }
+*/
+//clase 30/08
+//Objects
+let estudiante = {
+    nombre: Damaris,
+    apellido: Villegas,
+    DNI: 49333734,
+    direccion: nose
+}
+//mostrar en la consola
+console.log(estudiante.nombre + ' ' + estudiante.apellido)
+
+//para poner varios objetos se van separando con coma y se are con llaves
+//ej:
+let estudiante = {
+    nombre: 'Damaris',
+    apellido: 'Villegas',
+    dni: 49333734,
+    direccion: nose
+},
+{
+    nombre: 'Mica',
+    apellido: 'Cordoba',
+    dni: 29488894,
+    direccion: nose
+}
+
+//para mostrar los apellidos en la consola y o tener q escribir todos los datos usamos for
+for (let index = 0; index < estudiantes.length; index++) {
+    console.log(estudiantes[index].apellido + ' ' + estudiantes[index].nombre)
+}
+
+ //si quiero quee la consola busque un dato pongo:
+ estudiantes[1].apellido
+
+
