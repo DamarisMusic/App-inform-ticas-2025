ac418a8a3add2658fb8336cc282b6c5a51aa8ba9
alumno <alumno@Instituto.local>
2025-09-04 11:17:20 -0300

    dgdfr

--- pokeapi.js
+++ pokeapi.js
@@ -1,14 +1,13 @@
-//27/08725
-parrafo.textContent = 'pokemon'
-fetch('https://pokeapi.co/api/v2/pokemon?-limit-5')
+
+let img = document.querySelector('img')
+fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2008-08-10') //el &date=año-fecha-mes es en este caso para pedir una imagen de un año y fecha determinada, es decir, una imagen que se haya cargado en esa fecha. Count es para pedir un numero de objetos
 
 .then(respuesta => respuesta.json())
-
-.then(datos => {
-    console.log(datos.results)
-    for (let index = 0; index < datos.results.length; index++) {
-        //const element = datos.results[i];
-        parrafo.textContent = parrafo.textContent + ' ' + datos.results[i].name
-        console.log(datos.results[i].name)
+.then(datos =>{
+    console.log(datos)
+    img.src = datos.url
+    for (let index = 0; index < array.length; index++) {
+        contenedor.innerHTML += '<img src="' +datos[i].url+'" alt"">'
+        
     }
 })
